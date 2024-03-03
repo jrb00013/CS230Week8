19c9d25629e912f46d3915b84873cf372d4d386a
jrb00013 <156119500+jrb00013@users.noreply.github.com>
2024-03-03 15:18:30 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Card from './Card';
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 
 function App() {
